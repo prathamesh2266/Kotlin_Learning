1ec5fd0c6ed65e0cf6e03c4d75f66158d684daa1
Prathamesh Patil <prathameshpatil4855@gmail.com>
2026-04-15 01:42:32 +0530

    Implement shape interface and rectangle class
    Added interface and data class for shape and rectangle.

--- 5_interface.kt
+++ 5_interface.kt
@@ -1,1 +1,26 @@
+fun main(){
+  println(sumareas(rect1, rect2))     // calling the function sumareas to add the area of both objects
+}
 
+
+// DAy 5 Interface 😊
+interface shape {   // interface is the contract for classes
+    val area : Float
+    val circumference : Float
+}
+
+fun sumareas(vararg shapes : shape) : Double{   // we needed interface for adding both areas of the objects
+    return shapes.sumOf {   // double is used bcz sumOf do not accept float value
+        currentShape -> currentShape.area.toDouble()
+    }
+}
+
+data class rectangle(val width: Float, val height: Float): shape{  // Implement the shape interface
+
+    val w = width
+    val h = height
+    val diagonal = sqrt(w * w + h * h)  
+    override val area: Float = w * h   // overriding the function of the interface and it is compalsory to add into the class 
+    override val circumference: Float = 2 * (w +h)  // overriding the function of the interface and it is compalsory to add into the class
+
+}
