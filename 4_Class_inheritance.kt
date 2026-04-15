adb6e54b87bcd66f595c8f578cb75c2755065349
Prathamesh Patil <prathameshpatil4855@gmail.com>
2026-04-15 17:25:27 +0530

    Add 4_Class_inheritance.kt file

--- 4_Class_inheritance.kt
+++ 4_Class_inheritance.kt
@@ -1,4 +1,7 @@
-fun main(){
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+fun main() {
 
     // day 4 class and data class
     val rect1 = rectangle(width = 15F, height = 10F)  // Object one created
@@ -20,6 +23,24 @@ fun main(){
     println("rect2 is sqare? : " + rect2.issquare())
 
     println("Are both objects has same parameters: " + (rect1 == rect2))    // will give false as it compare if the both objects are same not the parameter (bcz we are using normal class not data class)
+
+
+    val circle1 = circle(5f)
+
+    println("area of circle : ${circle1.area}" )
+    println("area of circumference : ${circle1.circumference}" )
+}
+
+open class shape{   // created a shape class which will be inherited afterwords (need to declare as open class otherwise cannot  be inherited)
+    var count = 0   // if we do not declare variable as open, then it cannot be overriden but still can be used
+
+    fun increment(){    // this function can be accessed but cannot be overriden
+        count++
+    }
+
+    open val area: Float = 0f   // can be overriden as its open
+    open val circumference: Float = 0f
+
 }
 
 
@@ -31,8 +52,15 @@ class rectangle(width: Float, height: Float){
     val area = w * h
 
     fun issquare(): Boolean {
-          return w == h
-      }
+        return w == h
+    }
+}
+
+class circle(radius: Float): shape(){
+
+    val r = radius
+    override val area: Float = r*r* PI.toFloat()
+    override val circumference: Float = 2*r*PI.toFloat()
 }
 
 fun maxarea(rect1: rectangle, rect2: rectangle): Float {
